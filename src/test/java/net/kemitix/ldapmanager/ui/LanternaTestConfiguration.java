9bbf19fe74bcf2323d52cda5abe8b253fca1666e
Paul Campbell <pcampbell@kemitix.net>
2016-09-26 23:00:41 +0100

    LanternaTestConfiguration: add mock window and virtual terminal

---
+++ src/test/java/net/kemitix/ldapmanager/ui/LanternaTestConfiguration.java
@@ -0,0 +1,58 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 Paul Campbell
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package net.kemitix.ldapmanager.ui;
+
+import com.googlecode.lanterna.gui2.BasicWindow;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.virtual.DefaultVirtualTerminal;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.mock;
+
+/**
+ * Configuration for test.
+ *
+ * @author Paul Campbell (pcampbell@kemitix.net)
+ */
+@Configuration
+@Import({LanternaConfiguration.class})
+@Profile("test")
+public class LanternaTestConfiguration {
+
+    @Bean
+    Terminal terminal() throws IOException {
+        return new DefaultVirtualTerminal();
+    }
+
+    @Bean
+    BasicWindow window() {
+        return mock(BasicWindow.class);
+    }
+}
