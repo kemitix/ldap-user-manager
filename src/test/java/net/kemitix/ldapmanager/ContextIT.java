7f2ae659be69362b13e77dd1515641acff08f7f3
Paul Campbell <pcampbell@kemitix.net>
2016-09-28 21:09:04 +0100

    ContextIT: rename test class

--- src/test/java/net/kemitix/ldapmanager/ContextIT.java
+++ src/test/java/net/kemitix/ldapmanager/ContextIT.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
-public class LdapUserManagerApplicationTests {
+public class ContextIT {
 
     @Test
     public void contextLoads() {
