ab669b7181912d536f90308a2317d790037fab8a
Paul Campbell <pcampbell@kemitix.net>
2016-10-07 23:23:18 +0100

    DefaultLdapEntityContainerMapTest: test added methods

--- src/test/java/net/kemitix/ldapmanager/state/DefaultLdapEntityContainerMapTest.java
+++ src/test/java/net/kemitix/ldapmanager/state/DefaultLdapEntityContainerMapTest.java
@@ -7,6 +7,7 @@ import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ldap.support.LdapNameBuilder;
 
+import javax.naming.ldap.LdapName;
 import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,32 +22,61 @@ public class DefaultLdapEntityContainerMapTest {
     @InjectMocks
     private DefaultLdapEntityContainerMap containerMap;
 
+    private LdapName dn;
+
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
+        dn = LdapNameBuilder.newInstance("ou=users")
+                            .build();
     }
 
     @Test
-    public void shouldFindOrDefaultWhenNotPut() throws Exception {
-        //given
-        val name = LdapNameBuilder.newInstance("ou=users")
-                                  .build();
+    public void getShouldReturnEmptyIfNotFound() {
         //when
-        val result = containerMap.getOrCreate(name, key -> LdapEntityContainer.empty());
+        val result = containerMap.get(dn);
+        //then
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void getShouldReturnItemIfFound() {
+        //given
+        containerMap.put(dn, LdapEntityContainer.empty());
+        //when
+        val result = containerMap.get(dn);
+        //then
+        assertThat(result).isNotEmpty();
+        result.ifPresent(item -> assertThat(item).isSameAs(LdapEntityContainer.empty()));
+    }
+
+    @Test
+    public void getOrCreateShouldCreateWhenNotFound() throws Exception {
+        //when
+        val result = containerMap.getOrCreate(dn, key -> LdapEntityContainer.empty());
         //then
         assertThat(result).isSameAs(LdapEntityContainer.empty());
     }
 
     @Test
-    public void shouldFindOfDefaultWhenPut() throws Exception {
+    public void getOrCreateShouldFindWhenPut() throws Exception {
         //given
-        val name = LdapNameBuilder.newInstance("ou=users")
-                                  .build();
         val container = LdapEntityContainer.of(new ArrayList<>());
-        containerMap.put(name, container);
+        containerMap.put(dn, container);
         //when
-        val result = containerMap.getOrCreate(name, key -> LdapEntityContainer.empty());
+        val result = containerMap.getOrCreate(dn, key -> LdapEntityContainer.empty());
         //then
         assertThat(result).isSameAs(container);
+    }
+
+    @Test
+    public void clearShouldRemoveAll() throws Exception {
+        //given
+        containerMap.put(dn, LdapEntityContainer.empty());
+        assertThat(containerMap.get(dn)).isNotEmpty();
+        //when
+        containerMap.clear();
+        //then
+        assertThat(containerMap.get(dn)).isEmpty();
     }
 }
