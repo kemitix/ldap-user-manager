796e9d5a4494104f1082fe2bfd927ca22d426bdb
Paul Campbell <pcampbell@kemitix.net>
2016-10-14 22:37:18 +0100

    navigation.SelectionChangeListener: added

---
+++ src/main/java/net/kemitix/ldapmanager/navigation/SelectionChangeListener.java
@@ -0,0 +1,92 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 Paul Campbell
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package net.kemitix.ldapmanager.navigation;
+
+import lombok.extern.java.Log;
+import net.kemitix.ldapmanager.state.LogMessages;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Level;
+
+/**
+ * Listens for changes in selected navigation items.
+ *
+ * @author Paul Campbell (pcampbell@kemitix.net)
+ */
+@Log
+@Component
+class SelectionChangeListener {
+
+    private final LogMessages logMessages;
+
+    /**
+     * Constructor.
+     *
+     * @param logMessages The log messages.
+     */
+    SelectionChangeListener(final LogMessages logMessages) {
+        this.logMessages = logMessages;
+    }
+
+    /**
+     * Listener for {@link NavigationItemSelectionChangedEvent} to have the newly selected item republish itself with an
+     * appropriately typed event.
+     *
+     * @param event the event
+     */
+    @EventListener(NavigationItemSelectionChangedEvent.class)
+    public final void onSelectionChanged(final NavigationItemSelectionChangedEvent event) {
+        final String message = String.format("onSelectionChanged(%s -> %s)", event.getOldItem(), event.getNewItem());
+        log.log(Level.FINEST, message);
+        logMessages.add(message);
+        event.getNewItem()
+             .ifPresent(NavigationItem::publishAsSelected);
+    }
+
+    /**
+     * Listener for {@link NavigationItemOuSelectedEvent} to simply log the selection of the OU.
+     *
+     * @param event the event
+     */
+    @EventListener(NavigationItemOuSelectedEvent.class)
+    public final void onOuSelected(final NavigationItemOuSelectedEvent event) {
+        final String message = String.format("onOuSelected(%s)", event.getDn());
+        log.log(Level.FINEST, message);
+        logMessages.add(message);
+    }
+
+    /**
+     * Listener for {@link NavigationItemUserSelectedEvent} to simply log the selection of the User.
+     *
+     * @param event the event
+     */
+    @EventListener(NavigationItemUserSelectedEvent.class)
+    public final void onUserSelected(final NavigationItemUserSelectedEvent event) {
+        final String message = String.format("onUserSelected(%s)", event.getDn());
+        log.log(Level.FINEST, message);
+        logMessages.add(message);
+    }
+}
---
+++ src/test/java/net/kemitix/ldapmanager/navigation/SelectionChangeListenerTest.java
@@ -0,0 +1,80 @@
+package net.kemitix.ldapmanager.navigation;
+
+import lombok.val;
+import net.kemitix.ldapmanager.domain.OU;
+import net.kemitix.ldapmanager.domain.User;
+import net.kemitix.ldapmanager.state.LogMessages;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.context.ApplicationEventPublisher;
+
+import static org.mockito.BDDMockito.then;
+
+/**
+ * Tests for {@link SelectionChangeListener}.
+ *
+ * @author Paul Campbell (pcampbell@kemitix.net)
+ */
+public class SelectionChangeListenerTest {
+
+    private SelectionChangeListener listener;
+
+    @Mock
+    private LogMessages logMessages;
+
+    @Mock
+    private NavigationItem oldItem;
+
+    @Mock
+    private NavigationItem newItem;
+
+    @Mock
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        listener = new SelectionChangeListener(logMessages);
+    }
+
+    @Test
+    public void shouldOnSelectionChanged() throws Exception {
+        //given
+        val event = NavigationItemSelectionChangedEvent.of(oldItem, newItem);
+        //when
+        listener.onSelectionChanged(event);
+        //then
+        then(newItem).should()
+                     .publishAsSelected();
+    }
+
+    @SuppressWarnings("JUnitTestMethodWithNoAssertions")
+    @Test
+    public void shouldOnOuSelected() throws Exception {
+        //given
+        val ou = OU.builder()
+                   .build();
+        val ouItem = OuNavigationItem.create(ou, applicationEventPublisher);
+        val event = NavigationItemOuSelectedEvent.of(ouItem);
+        //when
+        listener.onOuSelected(event);
+        //then
+        //...method only does logging for now
+    }
+
+    @SuppressWarnings("JUnitTestMethodWithNoAssertions")
+    @Test
+    public void shouldOnUserSelected() throws Exception {
+        //given
+        val user = User.builder()
+                       .build();
+        val userItem = UserNavigationItem.create(user, applicationEventPublisher);
+        val event = NavigationItemUserSelectedEvent.of(userItem);
+        //when
+        listener.onUserSelected(event);
+        //then
+        //...method only does logging for now
+    }
+}
