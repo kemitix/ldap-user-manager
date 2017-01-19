ae471e28f86d978573887aca3f5f0661ab13a34e
Paul Campbell <pcampbell@kemitix.net>
2017-01-19 14:03:06 +0000

    RenameRequestEvent: convert to @RequiredArgsConstructor

--- src/main/java/net/kemitix/ldapmanager/actions/rename/RenameRequestEvent.java
+++ src/main/java/net/kemitix/ldapmanager/actions/rename/RenameRequestEvent.java
@@ -24,7 +24,9 @@ SOFTWARE.
 
 package net.kemitix.ldapmanager.actions.rename;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import javax.naming.Name;
 
@@ -33,14 +35,11 @@ import javax.naming.Name;
  *
  * @author Paul Campbell (pcampbell@kemitix.net)
  */
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public final class RenameRequestEvent {
 
     @Getter
     private final Name dn;
-
-    private RenameRequestEvent(final Name dn) {
-        this.dn = dn;
-    }
 
     /**
      * Create new RenameRequestEvent.
