e1f396a130d6db3459a90516ca8481ae1e036811
Paul Campbell <pcampbell@kemitix.net>
2017-02-09 21:44:43 +0000

    common: MessagesTest: add valuesOf test

--- common/src/test/java/net/kemitix/ldapmanager/MessagesTest.java
+++ common/src/test/java/net/kemitix/ldapmanager/MessagesTest.java
@@ -4,6 +4,8 @@ import net.kemitix.ldapmanager.ldap.LdapAttribute;
 import net.trajano.commons.testing.UtilityClassTestUtil;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Tests for {@link Messages}.
  *
@@ -14,5 +16,10 @@ public class MessagesTest {
     @Test
     public void shouldBeUtilityClass() throws Exception {
         UtilityClassTestUtil.assertUtilityClassWellDefined(LdapAttribute.class);
+    }
+
+    @Test
+    public void valuesOfCoverage() {
+        assertThat(Messages.valueOf(Messages.APP_NAME.toString())).isEqualTo(Messages.APP_NAME);
     }
 }
